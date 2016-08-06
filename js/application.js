94968ac029f9468c231b475747d6bdf56edc5ab2
Jazib Bashir <jazibbashir@gmail.com>
2016-08-06 12:45:29 +0500

    exter-studio-service-page - converted psd into html using bootstrap

---
+++ js/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.bxslider').bxSlider({
+	    slideWidth: 200,
+	    minSlides: 2,
+	    maxSlides: 6,
+	    slideMargin: 10,
+	    auto: true,
+	  	autoControls: false
+  	});
+});
